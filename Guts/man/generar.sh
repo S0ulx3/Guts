534086296d955341b4931849dfa5c2cc8fecf505
ShadoW <98002038+S0ulx3@users.noreply.github.com>
2024-12-17 18:05:37 +0100

    Update generar.sh
    Correción al error provocado por la falta de  pandoc  .
    Ahora el script detecta automáticamente si está instalado y si no es así lo instala y prosigue con el script.

--- Guts/man/generar.sh
+++ Guts/man/generar.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Comprobar si pandoc está instalado
+if ! which pandoc > /dev/null 2>&1; then
+
+    sudo apt-get update
+    sudo apt-get install -y pandoc
+else
+:
+fi
+
 for md in guts.*.md; do
   salida=${md//\.md/}
   pandoc "$md" -s -t man -o "$salida"
